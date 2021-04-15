e144daa9aeda12dbb98ac2c1a868d7cbd51e86a9
Antonio Castrovinci <a.castrovinci@nigithor.com>
2021-04-16 00:08:57 +0200

    Creazione del sistema storybook

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../projects/ds-storybook/stories/**/*.stories.mdx",
+    "../projects/ds-storybook/stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
